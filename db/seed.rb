cd638f03ae3b7d333cab5917d5a5c5af55de78bd
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-07-23 17:21:33 -0400

    Answered 5 query questions

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,26 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'sqlite3'
+require 'csv'
+require 'pry'
+
+db = SQLite3::Database.new ":memory:"
+
+#Create a database 
+rows = db.execute <<-SQL
+create table dailyshow (
+    season text,
+    profession text,
+    airdate text,
+    category text,
+    name text
+);
+SQL
+
+parsed_file = CSV.read("../daily_show_guests.csv")
+
+parsed_file.each do |line|
+    db.execute("insert into dailyshow values (?, ?, ?, ?, ?)", line[0], line[1], line[2], line[3], line[4])
+end
+
+db.execute("select * from dailyshow")
+binding.pry
--- lib/queries.rb
+++ lib/queries.rb
@@ -1,5 +1,18 @@
 # Write methods that return SQL queries for each part of the challeng here
-
+require_relative '../config/environment.rb'
+require 'pry'
 def guest_with_most_appearances
-  # write your query here!
+  db.execute("select name, count(name) from dailyshow group by name order by count(name) desc limit 1")[0]
 end
+
+def most_frequent_profession 
+  db.execute("select profession, count(profession) from dailyshow group by profession order by count(profession) desc limit 1")[0]
+end
+def all_guests_named_bill 
+  db.execute("select distinct name from dailyshow where name like 'Bill%'")[0]
+end
+
+
+
+binding.pry 
+
